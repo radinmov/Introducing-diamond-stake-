a02796b1f2769541298329d37951b421587d49cc
Radin Movahedi <radinmov@Radins-MacBook-Air.local>
2024-12-10 16:33:17 +0330

    the config file is added by ali in the radins device1

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Header from "../../componets/Header";
 
 function Home() {
@@ -10,7 +9,7 @@ function Home() {
         <div className="flex flex-col md:flex-row items-center gap-8 max-w-4xl mx-auto py-10">
           <div className="w-full md:w-1/3">
             <img
-              src="assets/icons/diamond.png"
+              src="assets/icons/diamond_300.png"
               alt="Book Cover"
               className="rounded-lg shadow-lg w-[200px]"
             />
@@ -18,31 +17,30 @@ function Home() {
 
           <div className="text-left w-full md:w-2/3">
             <h2 className="text-2xl md:text-4xl font-bold mb-4">
-              The Diamond stake application
+              The Diamond Stake Application
             </h2>
             <p className="text-gray-400 mb-6">
               This app belongs to BTS Contracting Company based in Canada, specializing in township development and hotel construction.
+              <br />
 
-              We invite you to join us in this venture, offering you the opportunity to earn profits in USD.
-
+              <br />
               Our goal is a long-term 10-year partnership, and there are plenty of exciting opportunities waiting for you in this investment.
             </p>
             <div className="flex gap-4">
-              <button className="bg-gray-800 px-6 py-2 rounded-lg hover:bg-gray-700">
-                Buy on Amazon
+              <button className="bg-black border border-lime-400 text-lime-400 px-6 py-2 rounded-lg shadow-md hover:bg-lime-400 hover:text-black transition duration-300">
+              Download for Android 
               </button>
-              <button className="bg-gray-800 px-6 py-2 rounded-lg hover:bg-gray-700">
-                Learn More
+              <button className="bg-black border border-cyan-400 text-cyan-400 px-6 py-2 rounded-lg shadow-md hover:bg-cyan-400 hover:text-black transition duration-300">
+              Download for Ios web 
               </button>
             </div>
           </div>
         </div>
 
-        {/* Cloud-like Section */}
         <div className="bg-gray-900 p-8 rounded-t-3xl mt-10">
           <div className="max-w-4xl mx-auto">
             <h3 className="text-xl md:text-2xl font-semibold mb-6">Table of Contents</h3>
-            <ul className="text-gray-400 list-disc pl-5 space-y-2">
+            <ul className="text-gray-400 list-disc pl-5 space-y-2 ">
               <li>The Struggle is Real</li>
               <li>The Danger of Misguided Values</li>
               <li>Identifying and Avoiding Shitty Values</li>
